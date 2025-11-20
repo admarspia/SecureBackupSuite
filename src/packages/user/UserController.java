8b7b9aeffd16f27cb2a4db60b8b1add859a87b03
Abrham Aragie <1admarspis@gmail.com>
2025-11-21 01:03:13 +0300

    Added UserController.Handles user input validation, and verification.

---
+++ src/packages/user/UserController.java
@@ -0,0 +1,81 @@
+package user;
+
+import exception.userservice.*;
+
+
+public class UserController {
+    private static UserService userService;
+
+    static { // bc static runs when the class is loadded.
+        try {
+            userService = new UserService();
+        } catch ( SQLException ex){
+            ex.printStackTrace();
+            throw new RuntimeException("Failed to Initialize UserService.");
+        }
+    }
+
+    public static void validateCredentials( boolean matched ) throws InvalidCredentialsException {
+        try {
+            if (!matched) throw new InvalidCredentialsException();
+
+        } catch ( InvalidCredentialsException ex) {
+            Logger.log("Error", "userservice", ex);
+            throw ex;
+        }
+
+    }
+
+    public static void validateUsername( String username) throws InvalidUsernameException, UsernameExistsException {
+        try {
+            if (username == null || username.length() < 3 || username.length() > 30) 
+                throw new InvalidUsernameException();
+
+            if (userService.getUser(username) != null)
+                throw new UsernameExistsException();
+
+        } catch (InvalidUsernameException  ex){
+                Logger.log("Error", "userservice", ex);
+                throw ex;
+        } catch (UsernameExistsException ex){
+            Logger.log("Error", "userservice", ex);
+            throw ex;
+        }
+
+    }
+
+    public static void validateEmail( String email ) throws InvalidEmailFormatException, EmailExistsException {
+        try {
+            String emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+
+            if (!email.matches(emailPattern)) 
+                throw new InvalidEmailFormatException();
+
+            if (userService.getUserByEmail(email) != null)
+                throw new EmailExistsException();
+
+
+        }catch (InvalidEmailFormatException ex) {
+            Logger.log("Error", "userservice", ex);
+            throw ex;
+        }catch (EmailExistsException ex){
+            Logger.log("Error", "userservice", ex);
+            throw ex;
+        }
+
+    }
+
+    public static void validatePassword( String password ) throws InvalidPasswordException {
+        try  {
+            if (password == null || password.length() < 8 || password.length() > 256 )
+                throw new InvalidPasswordException();
+
+        } catch (InvalidPasswordException ex){
+                Logger.log("Error", "userservice", ex);
+                throw ex;
+
+        }
+
+    }
+ 
+}
