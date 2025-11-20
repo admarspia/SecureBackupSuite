8aac72e85c2937eb829248dd66c032bfbd876f70
Abrham Aragie <1admarspis@gmail.com>
2025-11-21 01:00:24 +0300

    Added UserModel,contains unique username, email and hashedpassword, getters and setters

---
+++ src/packages/user/UserModel.java
@@ -0,0 +1,42 @@
+package user;
+
+import exception.userservice.*;
+
+public class UserModel {
+    private String username;
+    private String email;
+    private String passwordHash;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email)  {
+            this.email = email;
+
+    }
+
+    public void setPassword( String password) { 
+        this.passwordHash = EncryptionUtils.hashPassword(password);
+
+    }
+    public void setPasswordHash(String hashedPassword){
+        this.passwordHash = hashedPassword;
+
+    }
+
+    public String getUsername(){
+        return this.username;
+
+    }
+
+    public String getEmail(){
+        return this.email;
+
+    }
+
+    public String getPasswordHash(){
+        return this.passwordHash;
+    }
+
+}
