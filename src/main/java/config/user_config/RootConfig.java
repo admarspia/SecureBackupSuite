cdcdb6cc2a4c5b5cc70a4e9b73590684473dfc44
Abrham Aragie <1admarspis@gmail.com>
2025-12-02 19:52:36 +0300

    updated to support storage cofnig

--- src/main/java/config/user_config/RootConfig.java
+++ src/main/java/config/user_config/RootConfig.java
@@ -2,12 +2,14 @@ package config.user_config;
 
 import config.user_config.schedule_config.*;
 import config.user_config.file_config.*;
+import config.user_config.storage_config.StorageConfigModel;
 import config.YamlLoader;
 
 public class RootConfig {
 
     private BackupFilesConfigModel files;
     private BackupScheduleConfigModel schedule;
+    private StorageConfigModel storage;
 
     public BackupFilesConfigModel getFiles() {
         return files;
@@ -23,6 +25,14 @@ public class RootConfig {
 
     public void setSchedule(BackupScheduleConfigModel schedule) {
         this.schedule = schedule;
+    }
+
+    public StorageConfigModel getStorage(){
+        return storage;
+    }
+
+    public void setStorage(StorageConfigModel storage){
+        this.storage = storage;
     }
 }
 
