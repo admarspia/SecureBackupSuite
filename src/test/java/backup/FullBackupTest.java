c75ef14ef8b08457a2ec8c85ab2ec9500fd732f4
Abrham Aragie <1admarspis@gmail.com>
2025-12-12 22:11:09 +0300

    added backup pakcage that hadles backup logic

---
+++ src/test/java/backup/FullBackupTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import utils.*;
+import backup.Backup;
+
+
+public class FullBackupTest {
+
+    @Test
+    void testFullBackup() {
+        assertDoesNotThrow(() -> {
+            Backup.backup();
+        });
+    }
+}
